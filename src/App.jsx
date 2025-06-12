7225af30c8515618e62f726a3719f70c33a5c439
Aditya Raut <rautadityaj@gmail.com>
2025-06-12 15:54:09 +0530

    textarea

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import TextArea from './components/TextArea'
+import './components/TextArea.css'
+const App = () => {
+  return (
+    <TextArea/>
+  )
+}
+
+export default App
---
+++ src/components/TextArea.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import './TextArea.css'
+import { useState } from 'react'
+import Button from './Button'
+const TextArea = () => {
+
+     const [text,setText]=useState("")
+     const [previewtext,setPreviewtext]=useState("")
+     function handleOnchangeIntextArea(event)
+     {
+            setText(event.target.value)
+            setPreviewtext(event.target.value)
+     }
+
+     function handleUpperCase()
+     {
+        setPreviewtext(previewtext.toUpperCase())
+     }
+
+     function handleLowerCase()
+     {
+        setPreviewtext(previewtext.toLowerCase())
+     }
+  return (
+    <>
+    <div id="parent-container">
+        <div id="left">
+            <textarea
+                rows="10"
+                cols="50"
+                value={text}
+                onChange={handleOnchangeIntextArea}
+            >
+            </textarea>
+            <div id="button-container">
+                <Button text="Uppercase" handleClick={handleUpperCase}/>
+                <Button text="Lowercase" handleClick={handleLowerCase}/>
+            </div>
+
+
+        </div>
+
+         <div id="right">
+            <h3>Preview</h3>
+            <p>
+                {previewtext}
+            </p>
+            <span>Word Count : {text.trim().split(" ").length}</span>
+            <span>Time Taken To Read : {(text.trim().split(" ").length/60).toFixed(2)} minutes</span>
+            <span>Character Count : {(text.trim().split("").length)}</span>
+
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default TextArea
